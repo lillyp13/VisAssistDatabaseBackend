cb0254d45c28ff5ec85ee2d6cc52d29a6f819ce8
James McCartney <james@adconceptsllc.com>
2026-01-14 11:49:12 -0500

    Classes added, luna model db file added, plan notes updated

--- VisAssistDatabaseBackEnd/VisAssistDatabaseBackEnd/DataUtilities/PageUtilities.cs
+++ VisAssistDatabaseBackEnd/VisAssistDatabaseBackEnd/DataUtilities/PageUtilities.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace VisAssistDatabaseBackEnd.DataUtilities
 {
-    internal class DatabaseConnection
+    internal class PageUtilities
     {
     }
 }
---
+++ VisAssistDatabaseBackEnd/VisAssistDatabaseBackEnd/DataUtilities/ShapesUtilities.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VisAssistDatabaseBackEnd.DataUtilities
+{
+    internal class ShapesUtilities
+    {
+    }
+}
